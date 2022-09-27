1f920d32295c2fe6102bce494c4d87673139bf17
Muhtasim Munem Mugdho <muhtasim.mugdho@bjitgroup.com>
2022-09-27 10:47:05 +0600

    complete unit testing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
 import "./Login.scss";
@@ -5,15 +6,34 @@ const Login = () => {
   const [invisible, setInvisible] = useState("p-form__warning--visible");
   const [checkName, setCheckName] = useState("");
   const [checkPass, setCheckPass] = useState("");
+  const [user, setUser] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (checkName == "" || checkPass == "") {
       setInvisible("p-form__warning--visible");
     } else setInvisible("p-form__warning--invisible");
   }, [checkName, checkPass]);
+
+  const handleclick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      setUser(data);
+    } catch {
+      setError(true);
+    }
+    setLoading(false)
+  };
+
   return (
     <div className="p-login">
       <form className="p-form">
+      <span className="user">{user.name}</span>
         <div className="p-form__header">Login Here</div>
         <input
           placeholder="username"
@@ -29,8 +49,12 @@ const Login = () => {
           value={checkPass}
           onChange={(e) => setCheckPass(e.target.value)}
         />
-        <button className="p-form__button" disabled={!checkName || !checkPass}>
-          Login
+        <button
+          className="p-form__button"
+          disabled={!checkName || !checkPass}
+          onClick={handleclick}
+        >
+          {loading ? "please wait" : "Login"}
         </button>
         <span
           data-testid="error"
--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,11 +1,22 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Login from "./Login";
+
+jest.mock("axios", () => ({
+  __esModule: true,
+
+  default: {
+    get: () => ({
+      data: { id: 1, name: "John" },
+    }),
+  },
+}));
 
 test("username input should be rendered", () => {
   render(<Login />);
   const userInputEl = screen.getByPlaceholderText(/username/i);
   expect(userInputEl).toBeInTheDocument();
 });
+
 test("username input should be empty", () => {
   render(<Login />);
   const usernameInputEl = screen.getByPlaceholderText(/username/i);
@@ -17,6 +28,7 @@ test("password input should be rendered", () => {
   const passwordInputEl = screen.getByPlaceholderText(/password/i);
   expect(passwordInputEl).toBeInTheDocument();
 });
+
 test("password input should be empty", () => {
   render(<Login />);
   const passwordInputEl = screen.getByPlaceholderText(/password/i);
@@ -28,16 +40,19 @@ test("button input should be rendered", () => {
   const buttonInputEl = screen.getByRole("button");
   expect(buttonInputEl).toBeInTheDocument();
 });
+
 test("button should be disabled", () => {
   render(<Login />);
   const buttonInputEl = screen.getByRole("button");
   expect(buttonInputEl).toBeDisabled();
 });
+
 test("Text should not be visible", () => {
   render(<Login />);
   const errorEl = screen.getByTestId("error");
   expect(errorEl).toBeVisible();
 });
+
 test("username input should change", () => {
   render(<Login />);
   const usernameInputEl = screen.getByPlaceholderText(/username/i);
@@ -45,6 +60,7 @@ test("username input should change", () => {
   fireEvent.change(usernameInputEl, { target: { value: testValue } });
   expect(usernameInputEl.value).toBe(testValue);
 });
+
 test("password input should change", () => {
   render(<Login />);
   const passwordInputEl = screen.getByPlaceholderText(/password/i);
@@ -52,6 +68,7 @@ test("password input should change", () => {
   fireEvent.change(passwordInputEl, { target: { value: testValue } });
   expect(passwordInputEl.value).toBe(testValue);
 });
+
 test("button should not disabled", () => {
   render(<Login />);
   const buttonInputEl = screen.getByRole("button");
@@ -61,4 +78,51 @@ test("button should not disabled", () => {
   fireEvent.change(usernameInputEl, { target: { value: testValue } });
   fireEvent.change(passwordInputEl, { target: { value: testValue } });
   expect(buttonInputEl).not.toBeDisabled();
+});
+
+test("loading should be rendered when click", () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = "test";
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+
+  expect(buttonEl).toHaveTextContent(/please wait/i);
+});
+
+test("loading should not be rendered after fetching", async () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = "test";
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+
+  await waitFor(() => expect(buttonEl).not.toHaveTextContent(/please wait/i));
+});
+
+test("user should be rendered after fetching", async () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = "test";
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+
+  const userItem = await screen.findByText("John");
+
+  expect(userItem).toBeInTheDocument();
 });
